a2ae64e788898ea9c8802a87b92edd9793956a72
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2023-01-25 12:52:39 -0500

    2023

--- SimulateGame.py
+++ SimulateGame.py
@@ -1,16 +1,16 @@
-import GameClasses as Cls
-import SimPy.Plots.Histogram as Hist
+import deampy.plots.histogram as hist
 
+import GameClasses as Cls
 
 fair_games = Cls.SetOfGames(id=0, prob_head=0.5)
 fair_games.simulate(n_games=1000)
 print('Expected reward when the probability of head is 0.5:', fair_games.get_ave_reward())
 
 # plot the histogram
-Hist.plot_histogram(
+hist.plot_histogram(
     data=fair_games.gameRewards,
     title='Fair Game',
-    bin_width=50,
+    bin_width=100,
     x_label='Game Rewards',
     y_label='Count')
 
@@ -23,10 +23,10 @@ bias_games.simulate(n_games=1000)
 print('Expected reward when the probability of head is 0.4:', bias_games.get_ave_reward())
 
 # plot the histogram
-Hist.plot_histogram(
+hist.plot_histogram(
     data=bias_games.gameRewards,
     title='Biased Game (Probability of Head is 0.4)',
-    bin_width=50,
+    bin_width=100,
     x_label='Game Rewards',
     y_label='Count')
 
